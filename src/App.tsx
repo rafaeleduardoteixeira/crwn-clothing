3aa9bd97bd58bb75bb6322308d2176c3f8538380
Rafael Teixeira <rafaelteixeira@Rafaels-MacBook-Pro.local>
2023-02-03 08:40:27 -0300

    chore: add alias to project

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
-import React from "react";
+import { categories } from './constants';
+
+import './categories.styles.scss';
 
 function App() {
-  return <div>Hello World!</div>;
+  return (
+    <div className="categories-container">
+      {categories.map(({ id, title, imageUrl }) => (
+        <div key={id} className="category-container">
+          <div className="background-image" />
+          <div className="category-body-container">
+            <h2>{title}</h2>
+            <p>Shop Now</p>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default App;
