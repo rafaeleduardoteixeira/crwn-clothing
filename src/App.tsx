1589416b3391fd36ba2b52d19cd0a1353e4a2b0b
Rafael Teixeira <rafaelteixeira@Rafaels-MacBook-Pro.local>
2023-02-07 16:31:00 -0300

    feature: create a initial structure to navigation bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
-// Styles
+import { Routes, Route } from 'react-router-dom';
+
 // Components
 import Home from 'components/routes/home/home';
+import Navigation from 'components/navigation/navigation';
 
 // Styles
 import './App.scss';
 
 function App() {
-  return <Home />;
+  return (
+    <Routes>
+      <Route path="/" element={<Navigation />}>
+        <Route index element={<Home />} />
+      </Route>
+    </Routes>
+  );
 }
 
 export default App;
