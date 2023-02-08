c25ff7cfb7840e3031f73332f2b59505e08f8adb
Rafael Teixeira <rafaelteixeira@Rafaels-MacBook-Pro.local>
2023-02-08 16:45:13 -0300

    feature: create a nav menu with logo

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,18 @@ import { Routes, Route } from 'react-router-dom';
 
 // Components
 import Home from 'components/routes/home/home';
-import Navigation from 'components/navigation/navigation';
+import Navigation from 'components/routes/navigation/navigation';
 
 // Styles
 import './App.scss';
+import Shop from 'components/routes/shop/shop';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Navigation />}>
         <Route index element={<Home />} />
+        <Route path="shop" element={<Shop />} />
       </Route>
     </Routes>
   );
